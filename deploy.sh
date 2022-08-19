bae631c56b64ca6943a66743dba4d50fbc5c6d22
atheo89 <atheodorak@outlook.com>
2022-08-19 11:09:44 +0200

    Added monitoring target for Jupyter notebook spawner page

--- deploy.sh
+++ deploy.sh
@@ -219,7 +219,11 @@ oc apply -f monitoring/rhods-dashboard-route.yaml -n $ODH_PROJECT
 
 rhods_dashboard_host=$(oc::wait::object::availability "oc get route rhods-dashboard -n $ODH_PROJECT -o jsonpath='{.spec.host}'" 2 30 | tr -d "'")
 
+NOTEBOOK_SUFFIX="/notebookController/spawner"
+notebook_spawner_host=$(oc::wait::object::availability "oc get route rhods-dashboard -n $ODH_PROJECT -o jsonpath='{.spec.host}'$NOTEBOOK_SUFFIX'" 2 30 | tr -d "'")
+
 sed -i "s/<rhods_dashboard_host>/$rhods_dashboard_host/g" monitoring/prometheus/prometheus-configs.yaml
+sed -i "s/<notebook_spawner_host>/$notebook_spawner_host/g" monitoring/prometheus/prometheus-configs.yaml
 sed -i "s/<pagerduty_token>/$pagerduty_service_token/g" monitoring/prometheus/prometheus-configs.yaml
 sed -i "s/<set_alertmanager_host>/$alertmanager_host/g" monitoring/prometheus/prometheus.yaml
 
